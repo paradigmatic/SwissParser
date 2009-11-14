aa36a448243a1fc651d0cf7c6be8e96d903f40ff
JL Falcone <jean-luc.falcone@unige.ch>
2009-11-14 17:36:47 +0100

    Now compatible with ruby 1.9

--- lib/swissparser.rb
+++ lib/swissparser.rb
@@ -19,7 +19,7 @@ along with SwissParser.  If not, see <http://www.gnu.org/licenses/>.
 
 module Swiss
 
-  VERSION = "0.8.0"
+  VERSION = "0.8.1"
 
   # This class defines parsing rules. Its methods
   # are accessible within the +rules+ section of
@@ -77,24 +77,27 @@ module Swiss
     
     include InstanceExecHelper
 
-    #Used to execute rules and action using the ParsingContext as context
-    #Stolen from http://eigenclass.org/hiki/bounded+space+instance_exec
-    def instance_exec(*args, &block)
-      begin
-        old_critical, Thread.critical = Thread.critical, true
-        n = 0
-        n += 1 while respond_to?(mname="__instance_exec#{n}")
-        InstanceExecHelper.module_eval{ define_method(mname, &block) }
-      ensure
-        Thread.critical = old_critical
+    #Method instance_exec exists since version 1.9
+    if RUBY_VERSION < "1.9"
+      #Used to execute rules and action using the ParsingContext as context
+      #Stolen from http://eigenclass.org/hiki/bounded+space+instance_exec
+      def instance_exec(*args, &block)
+        begin
+          old_critical, Thread.critical = Thread.critical, true
+          n = 0
+          n += 1 while respond_to?(mname="__instance_exec#{n}")
+          InstanceExecHelper.module_eval{ define_method(mname, &block) }
+        ensure
+          Thread.critical = old_critical
+        end
+        begin
+          ret = send(mname, *args)
+        ensure
+          InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
+        end
+        ret
       end
-      begin
-        ret = send(mname, *args)
-      ensure
-        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-      end
-      ret
-    end 
+    end
     
   end
 
